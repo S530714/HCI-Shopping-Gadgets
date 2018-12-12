a22df1620cd7ab384142a7f800f1aac3ea11c3df
S530714 <S530714@nwmissouri.edu>
2018-12-12 16:04:14 -0600

    Shopping Gadgets in angular

---
+++ frontend/src/app/product/product.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { NgForm } from '@angular/forms';
+import { DataService } from '../services/dataservice/dataservice.service';
+import { NotificationService } from '../services/notification/notification.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  @ViewChild('f') updateproduct: NgForm;
+  constructor(
+    private dialog: MatDialogRef<ProductComponent>,
+    private ns:NotificationService,
+    @Inject(MAT_DIALOG_DATA) public data) { }
+    ngOnInit() { 
+      if(!this.data){
+        this.data = {
+          name: '',
+          description: '',
+          price: ''
+        }
+      }
+    }
+
+  onSubmit () {
+    this.dialog.close(new FormData(document.querySelector('#addproduct')))
+    this.ns.success('updated')
+  }
+
+}
---
+++ frontend/src/app/services/dialog/dialog.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { MatConfirmDialogComponent } from 'src/app/mat-confirm-dialog/mat-confirm-dialog.component';
+import { ProductComponent } from 'src/app/product/product.component';
+import { Product } from 'src/app/models/product.model';
+import { ProfileEditComponent } from 'src/app/profile-edit/profile-edit.component';
+import { Profile } from 'src/app/models/profile.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+
+  constructor(private dialog: MatDialog) { }
+  opendeletedialog () {
+    return this.dialog.open(MatConfirmDialogComponent, {
+      width: '390px',
+      panelClass: 'confirm-dialog-container',
+      position: { top: "10%" },
+      data :{
+        message : "Are you sure to delete this record ?"
+      }
+    })
+  }
+
+  openEditDialog (product: Product) {
+    this.dialog.open(ProductComponent, {
+      width: '40%',
+      height: '70%',
+      position: { top: "10%" },
+      data: product
+    })
+  }
+  openProfileEditDialog (profile: Profile) {
+    return this.dialog.open(ProfileEditComponent, {
+      width: '40%',
+      height: '75%',
+      position: { top: "10%" },
+      data: profile
+    })
+  }
+  opencreateproduct () {
+    return this.dialog.open(ProductComponent, {
+      width: '40%',
+      height: '70%',
+      position: { top: "10%" }
+    })
+  }
+}
